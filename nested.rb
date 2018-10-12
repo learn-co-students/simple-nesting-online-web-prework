d92605aa6ad8a42c7b640dd9827deb3ab9469b18
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-12 23:21:52 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages: ["Lisp", "C"]
+  }
+  
+  return programmer_hash
     
 end
 
@@ -101,7 +111,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
+     return programmer_hash
      
 end
 
@@ -122,4 +134,7 @@ def adding_to_dennis
         }
      }
 
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  
+  return programmer_hash
 end
