cfcf54b8b1f10d5015b5423c1d16c03bec148bad
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-09 23:07:46 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,9 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+hopper = programmer_hash[:grace_hopper]
 end
+
+
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +40,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    alan_kay_is_known_for = programmer_hash[:alan_kay][:known_for] 
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -79,8 +84,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
@@ -102,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
 end
 
 def adding_to_dennis
