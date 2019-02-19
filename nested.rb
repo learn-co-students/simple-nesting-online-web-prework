70c2ab77651ae32f3ab35cb62e6076e6fab929e0
Kat B <katreesebarnes@gmail.com>
2019-02-19 18:19:13 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,5 @@
 
+require "pry"
 def hopper
 	programmer_hash = 
  		{
@@ -104,7 +105,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     
+     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     programmer_hash
      
      
 end
@@ -125,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+      languages = programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+      programmer_hash
 end
