633688ba15b0bbd8652c855bff7f341c2a46e252
Kyle Rosipko <krosipko@outlook.com>
2019-03-02 23:30:38 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 def hopper
 	programmer_hash = 
  		{
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  new_array = []
+  new_array << programmer_hash[:dennis_ritchie][:languages][0]
+  new_array[0]
 end
 
 def adding_matz
@@ -79,8 +83,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+#    programmer_hash = {yukihiro_matsumoto: ""}
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    programmer_hash
 end
 
 def changing_alan
@@ -100,8 +106,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
      
 end
 
@@ -120,6 +126,7 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+    }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
