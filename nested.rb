b924debf83c0d7c5ea3037d8db998c133c77953a
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-10 20:56:55 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,17 +57,14 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    new_array = programmer_hash[:dennis_ritchie][:languages]
+    return new_array.join
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
- 		{
+ 		{   
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,8 +78,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash
 end
 
 def changing_alan
@@ -99,9 +101,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+   programmer_hash[:alan_kay][:known_for] = "GUI"
+   programmer_hash
      
 end
 
@@ -121,5 +122,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+     programmer_hash
 
 end
