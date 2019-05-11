fb724b0fb78c3bbabadcb1a82ecaf7b342793adf
Rene Cocom <fasnimo@gmail.com>
2019-05-11 06:13:24 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,9 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+      
+    programmer_hash[:grace_hopper]
+    
 
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +40,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +56,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,7 +84,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+   programmer_hash = {:yukihiro_matsumoto =>{}}
     
 end
 
@@ -99,8 +104,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+    # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+    # alans_new_info = "GUI"
+     
+     programmer_hash = {:alan_kay => {:known_for => "GUI"}}
      
      
 end
@@ -121,5 +128,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash = {:dennis_ritchie => {languages:["C", "Assembly"] }}
 end
